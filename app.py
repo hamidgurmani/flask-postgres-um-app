8075d53829cdd4e6155a2f659765d7b3a0ff8b6c
Hamid Jalil <hamid.ciit.09@gmail.com>
2026-02-20 13:45:49 +0500

    Fix lint errors for CI/CD

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ from datetime import datetime
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "dev-secret-key")
 
-# ----Database configuration----
+# ---- Database configuration ----
 DATABASE_URL = os.environ.get(
     "DATABASE_URL",
-   # "sqlite:///app.db"
     "postgresql://postgres:postgres@db:5432/flaskdb"
 )
 
@@ -18,11 +17,13 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+
 # ---- Model ----
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 # ---- Routes ----
 @app.route("/", methods=["GET", "POST"])
@@ -43,6 +44,7 @@ def index():
     users = User.query.all()
     return render_template("index.html", users=users)
 
+
 @app.route("/delete/<int:user_id>", methods=["POST"])
 def delete_user(user_id):
     user = User.query.get_or_404(user_id)
@@ -51,20 +53,13 @@ def delete_user(user_id):
     flash("User deleted ❌", "danger")
     return redirect(url_for("index"))
 
-# ---- App startup (IMPORTANT PART) ----
-#with app.app_context():
- #   db.create_all()
-
-# ---- Local dev only ----
-#if __name__ == "__main__":
- #   with app.app_context():
-  #      db.create_all()
-   # app.run(host="0.0.0.0", port=5000)
 
 # ---- Ensure DB tables exist (Docker / Gunicorn safe) ----
 with app.app_context():
     db.create_all()
 
+
+# ---- Local dev only ----
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)   
-    
+    app.run(host="0.0.0.0", port=5000)
+
